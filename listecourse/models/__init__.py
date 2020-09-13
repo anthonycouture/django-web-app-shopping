ef8cd97657e73e4e9a750a9b76b302afc3e82f11
Anthony Couture <antobox28@gmail.com>
2020-09-13 11:25:48 +0200

    mcd complet

--- listecourse/models/__init__.py
+++ listecourse/models/__init__.py
@@ -1,1 +1,2 @@
 from .produit import *
+from .liste import *
---
+++ listecourse/models/liste.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+from listecourse.models import Produit
+
+
+class Liste (models.Model):
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("produit", "utilisateur"),)
