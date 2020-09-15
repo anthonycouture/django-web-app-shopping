286b0ad9e81753816463ea34a2d7a6d854ca26e4
Anthony Couture <antobox28@gmail.com>
2020-09-16 01:26:47 +0200

    add correction6

--- listecourse/urls.py
+++ listecourse/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('logout', logout.index, name='logout'),
     path('registration', registration.index, name='registration'),
     path('addProduct', addProduct.index, name='addProduct'),
-    path('addProductInList', addProductInList.index, name='addProductInList')
+    path('addProductInList', addProductInList.index, name='addProductInList'),
+    path('deleteProductList/<str:nom_produit>', accueil.deleteProduit, name='deleteProductList'),
+    path('moreQuantite/<str:nom_produit>', accueil.moreQuantite, name='moreQuantite'),
+    path('lessQuantite/<str:nom_produit>', accueil.lessQuantite, name='lessQuantite'),
 ]
--- listecourse/views/accueil.py
+++ listecourse/views/accueil.py
@@ -1,9 +1,43 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from listecourse.models import Liste
+from listecourse.models import Liste, Produit
+from django.urls import reverse
+
 
 @login_required
 def index(request):
     user = request.user
     context = {'liste': Liste.objects.all().filter(utilisateur=user)}
     return render(request, 'listecourse/accueil.html', context)
+
+
+@login_required
+def deleteProduit(request, nom_produit):
+    user = request.user
+    produit = Produit.objects.get(nomProduit=nom_produit)
+    Liste.delete(Liste.objects.get(utilisateur=user, produit=produit))
+    return HttpResponseRedirect(reverse('accueil'))
+
+
+@login_required
+def moreQuantite(resquest, nom_produit):
+    user = resquest.user
+    produit = Produit.objects.get(nomProduit=nom_produit)
+    liste = Liste.objects.get(utilisateur=user, produit=produit)
+    liste.quantite = liste.quantite + 1
+    liste.save()
+    return HttpResponseRedirect(reverse('accueil'))
+
+
+@login_required
+def lessQuantite(resquest, nom_produit):
+    user = resquest.user
+    produit = Produit.objects.get(nomProduit=nom_produit)
+    liste = Liste.objects.get(utilisateur=user, produit=produit)
+    if liste.quantite == 1:
+        Liste.delete(liste)
+    else:
+        liste.quantite = liste.quantite - 1
+        liste.save()
+    return HttpResponseRedirect(reverse('accueil'))
